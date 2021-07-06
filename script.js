0f9a750467edcb6c8768cc582b9b23084d9abd4f
tatianakharitonovich <k.tanya@tut.by>
2021-07-06 10:53:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const burger = document.querySelector('.header__burger_button'),
 blockNav = document.querySelector('.header__nav'),
-navLink = document.querySelectorAll('.header__link');
-
+navLink = document.querySelectorAll('.header__link'),
+button= document.querySelectorAll('.main__article_button'),
+articleText = document.querySelectorAll('.main__article_text'),
+articleTitle = document.querySelectorAll('.main__article_title');
 
 burger.addEventListener('click', ()=> {
 	burger.classList.toggle ('header__burger_button_click'); 
@@ -13,6 +15,16 @@ blockNav.addEventListener('click',close);
 
 navLink.forEach (item => {item.addEventListener('click', close)});
 
+button.forEach ((item, index) => item.onclick = openText.bind(this, index));
+
+articleTitle.forEach ((item, index) => item.onclick = openText.bind(this, index));
+
+function openText (index) {
+	button[index].classList.toggle('main__article_button_rotate');
+	articleText[index].classList.toggle('main__article_text_open');
+	console.log(button[index]);
+}
+	
 function close() {
 		navLink.forEach (item => item.classList.remove ('header__link_click'));
 		burger.classList.remove ('header__burger_button_click');
