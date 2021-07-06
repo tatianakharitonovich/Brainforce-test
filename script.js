cfe98eab664b4772288f9aa81169814308d46ebc
tatianakharitonovich <k.tanya@tut.by>
2021-07-06 11:49:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function (callback, thisArg) {
+    var T, k;
+    if (this == null) {
+      throw new TypeError(' this is null or not defined');
+    }
+    var O = Object(this);
+    var len = O.length >>> 0;
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+    }
+    if (arguments.length > 1) {
+      T = thisArg;
+    }
+    while (k < len) {
+      var kValue;
+      if (k in O) {
+        kValue = O[k];
+        callback.call(T, kValue, k, O);
+      }
+      k++;
+    }
+  };
+}
+
 const burger = document.querySelector('.header__burger_button'),
 blockNav = document.querySelector('.header__nav'),
 navLink = document.querySelectorAll('.header__link'),
